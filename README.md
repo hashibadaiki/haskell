8e1edb9e94ef87cac8e862b0d856dfaff68c8247
hashibadaiki <higemeganeking@gmail.com>
2021-02-05 23:26:28 +0900

    2章まで読んだ

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # start haskell!!
 
-Haskellの練習リポジトリ
+Haskell の練習リポジトリ
+
+## REPL の起動
+
+`ghci`
+
+## REPL の終了
+
+`:quit`
+
+## ファイルの実行
+
+`ghc ファイル名.hs`
+
+## REPL 内でファイルをロードする
+
+`Prelude> :l hoge.hs`
+
+`*Main>`
+
+ロードする前はプロンプトが
+
+`Prelude>`
+
+となっていて、ロードすると、プロンプトが以下のようになる。
+
+`*Main>`
+
+ロードしたファイルを再ロードする際は、以下のように行う。
+
+`*Main> :l hoge.hs`
--- baby.hs
+++ baby.hs
@@ -3,3 +3,12 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
                     then x
                     else x * 2
+
+test xs = [if x < 10 then "boon" else "bang" | x <- xs]
+
+-- fizzbuzz 後で書く
+fuga xs = [if x < 10 then "boon" else "bang" | x <- xs]
+
+hoge xx= [if xx == 1 then xx + 1 else xx| y <- xx]
+
+triples = [(a,b,c)|c<-[1..10], a<-[1..c], b<-[1..a], a^2 + b^2 ==c^2,a+b+c==24]

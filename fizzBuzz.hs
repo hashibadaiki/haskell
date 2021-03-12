132acbe77bea89b3106798bd3239696416951abc
hashibadaiki <higemeganeking@gmail.com>
2021-03-12 23:26:24 +0900

    fizzbuzz途中まで

--- fizzBuzz.hs
+++ fizzBuzz.hs
@@ -1,14 +1,9 @@
--- fizzbuzzの組み立て
--- 1~100を順番に表示する
--- 引数の値に応じた処理をする関数
--- 他の言語はswitchとかやけど、haskellは違うかも
+checkFizzBuzz i x y
+  | i `mod` x == 0 && i `mod` y == 0 = putStrLn "fizzBuzz"
+  | i `mod` y == 0 = putStrLn "fizz"
+  | i `mod` x == 0 = putStrLn "buzz"
+  | otherwise = i
 
-add :: Int -> Int -> Int
-add x y = x + y
-
-total x = x + 1
-
--- 関数の引数として関数を渡せた
--- 型はとりあえず勝手につく。慣れるまでは推論させたほうがいいかも
--- 型宣言は独特。最初大文字
-main = print $ total (add 5 6)
+fizzBuzz i max x y
+  | i == max = putStrLn "end"
+  | i < max = checkFizzBuzz i x y

d01e7a0a6c0bbfd53d69559751f7eb8946897384
hashibadaiki <higemeganeking@gmail.com>
2021-03-20 00:27:58 +0900

    fizzBuzz途中まで

--- fizzBuzz.hs
+++ fizzBuzz.hs
@@ -1,9 +1,26 @@
-checkFizzBuzz i x y
-  | i `mod` x == 0 && i `mod` y == 0 = putStrLn "fizzBuzz"
-  | i `mod` y == 0 = putStrLn "fizz"
-  | i `mod` x == 0 = putStrLn "buzz"
-  | otherwise = i
+-- todo 同列に表示させたい
+-- todo 空行を消したい
+-- todo もう少しmainを短く書けそう
 
-fizzBuzz i max x y
-  | i == max = putStrLn "end"
-  | i < max = checkFizzBuzz i x y
+checkFizzBuzz i
+  | i `mod` 5 == 0 && i `mod` 3 == 0 = putStrLn "fizzBuzz"
+  | i `mod` 5 == 0 = putStrLn "fizz"
+  | i `mod` 3 == 0 = putStrLn "buzz"
+  | otherwise = putStrLn ""
+
+checkLimit i max
+  | i < max = do
+    print i
+    checkFizzBuzz i
+    checkLimit (i + 1) max
+  | i > max = print i
+  | otherwise = putStrLn "error"
+
+-- fib途中まで
+fib crr tol max
+  | crr > max = print ""
+  | crr < max = do
+    let prev = crr
+    let total = tol + prev
+    print total
+    fib prev total max

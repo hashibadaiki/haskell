148039fea1db454cac53836f67ef2cd5676fd494
hashibadaiki <higemeganeking@gmail.com>
2021-04-09 22:49:39 +0900

    フィボナッチ仮

--- baby.hs
+++ baby.hs
@@ -10,3 +10,21 @@ test xs = [if x < 10 then "boon" else "bang" | x <- xs]
 fuga xs = [if x < 10 then "boon" else "bang" | x <- xs]
 
 triples = [(a,b,c)|c<-[1..10], a<-[1..c], b<-[1..a]]
+
+
+-- フィボナッチ数列書いてみる
+-- function fib(n) {
+--  if (n < 3) return 1;
+--  return fib(n - 1) + fib(n - 2);
+-- }
+
+-- fib 0 = 0
+-- fib 1 = 1
+-- fib n = fib (n - 2) + fib (n - 1)
+
+fib :: (Num a, Num p, Ord a) => a -> p
+fib n
+    | n == 0 = 0
+    | n == 1 = 1
+    | 2 <= n = fib (n - 2) + fib (n - 1)
+    | otherwise = 100
